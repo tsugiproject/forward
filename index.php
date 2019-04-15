7e482ff4db0767d99ff9c685f08de0fd9c6a0681
Charles Severance <csev@umich.edu>
2019-04-14 22:01:04 -0400

    Add RPC bits

--- index.php
+++ index.php
@@ -4,8 +4,12 @@ require_once "../config.php";
 require_once "key.php";
 
 use \Tsugi\Core\LTIX;
+use \Tsugi\Util\U;
 use \Firebase\JWT\JWT;
 use \Tsugi\Util\LTI13;
+
+use \Tsugi\Crypt\Aes;
+use \Tsugi\Crypt\AesCtr;
 
 // Handle all forms of launch
 $LTI = LTIX::requireData();
@@ -13,6 +17,19 @@ $LTI = LTIX::requireData();
 $json = $_SESSION['lti'];
 $jwt_claim = LTI13::base_jwt("iss","subj");
 $jwt_claim["lti"] = $json;
+
+$endpoint = $CFG->wwwroot . "/api/rpc.php";
+
+$token = $CFG->cookiepad . '::' . session_id();
+
+$token = AesCtr::encrypt($token, $CFG->cookiesecret, 256) ;
+
+$callback = array('endpoint' => addSession($endpoint),
+    'token' => $token );
+
+$jwt = LTI13::encode_jwt($jwt_claim, $PRIVATE_KEY);
+
+$jwt_claim["callback"] = $callback;
 
 $jwt = LTI13::encode_jwt($jwt_claim, $PRIVATE_KEY);
 
